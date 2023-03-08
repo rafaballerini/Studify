4c8dbe4badc408abf7b2941415e7d4c2671f9fbd
renildomarcio <krsolucoesweb@gmail.com>
2023-03-07 22:39:07 -0300

    Update ping.js
    Nessa melhoria, o comando ping foi atualizado para exibir a latência do bot e da API do Discord. Além disso, a resposta foi transformada em um MessageEmbed personalizado e mais informativo. O código agora envia uma mensagem inicial "Pinging..." e depois edita a mensagem com o resultado da latência e o MessageEmbed.

--- commands/ping.js
+++ commands/ping.js
@@ -1,11 +1,20 @@
-const { SlashCommandBuilder } = require("discord.js")
+const { SlashCommandBuilder, MessageEmbed } = require("discord.js")
 
 module.exports = {
     data: new SlashCommandBuilder()
         .setName("ping")
-        .setDescription("Responde com 'Pong!"),
+        .setDescription("Verifica a latência do bot"),
 
     async execute(interaction) {
-        await interaction.reply("Pong!")
+        const sent = await interaction.reply({ content: 'Pinging...', fetchReply: true })
+        const timeDiff = (sent.editedAt || sent.createdAt) - (interaction.editedAt || interaction.createdAt)
+        const pingEmbed = new MessageEmbed()
+            .setColor('BLUE')
+            .setTitle('🏓 Pong!')
+            .addFields(
+                { name: 'Latência do Bot:', value: `${timeDiff}ms` },
+                { name: 'Latência da API:', value: `${Math.round(interaction.client.ws.ping)}ms` }
+            )
+        await interaction.editReply({ content: '\u200b', embeds: [pingEmbed] })
     }
 }
